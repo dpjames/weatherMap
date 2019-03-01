509c8eafac5de1f89a52d69fb8ebf867a18cc247
dpjames <david.patrick.james97@gmail.com>
2019-03-01 12:47:19 -0800

    more data

--- src/server.go
+++ src/server.go
@@ -7,20 +7,26 @@ import (
    "io/ioutil"
 )
 const PORT string = ":8080";
-func serverWeather(w http.ResponseWriter, r *http.Request){
+func serveWeather(w http.ResponseWriter, r *http.Request){
    fmt.Println("weather");
    data,_:=ioutil.ReadFile("outfile.geojson");
    fmt.Fprint(w,string(data))
 }
-func serverPeaks(w http.ResponseWriter, r *http.Request){
+func servePeaks(w http.ResponseWriter, r *http.Request){
    fmt.Println("peaks");
    data,_:=ioutil.ReadFile("peaks.geojson");
    fmt.Fprint(w,string(data))
 }
+func serveClimbs(w http.ResponseWriter, r *http.Request){
+   fmt.Println("climbs");
+   data,_:=ioutil.ReadFile("climbs.geojson");
+   fmt.Fprint(w,string(data))
+}
 func main() {
    fs := http.FileServer(http.Dir("public"))
-   http.HandleFunc("/weather", serverWeather);
-   http.HandleFunc("/peaks", serverPeaks);
+   http.HandleFunc("/weather", serveWeather);
+   http.HandleFunc("/peaks", servePeaks);
+   http.HandleFunc("/climbs", serveClimbs);
    http.Handle("/", fs)
    fmt.Println("Listening...");
    err := http.ListenAndServe(PORT, nil)

4fe78b7f562020cfd41a832c79b9801a04dd2bfd
dpjames <david.patrick.james97@gmail.com>
2019-03-02 19:08:00 -0800

    updated cleanWeather... might work now, but js needs to be updated too

--- util/cleanWeather.py
+++ util/cleanWeather.py
@@ -10,6 +10,14 @@ weather = json.loads(txt)
 for f in weather['features']:
     newF = f
     newF['geometry'] = f['geometry']['geometries'][0]
+    del newF['@context']
+    fp = f['properties']
+    for i in range(len(fp['periods'])):
+        cp = fp['periods'][i]
+        newF['properties']['isDaytime' + str(i)] = cp['isDaytime']
+        newF['properties']['name' + str(i)] = cp['name']
+        newF['properties']['shortForecast' + str(i)] = cp['shortForecast']
+        newF['properties']['startTime' + str(i)] = cp['startTime']
     newFeats.append(newF);
 
 weather['features'] = newFeats
